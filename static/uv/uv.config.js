9fe9920c255698b5aa4ec6f39e9e193d075c4896
DANtheHANDYMAN <dday.dwight@gmail.com>
2022-09-18 10:10:17 -0500

    eeeeeee

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -8,3 +8,66 @@ self.__uv$config = {
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
+
+//https://github.com/caracal-js/playground
+//https://github.com/caracal-js/Incognito
+//https://github.com/titaniumnetwork-dev/Ultraviolet-Node
+//https://github.com/caracal-js/gfiles/blame/main/retro-bowl/index.html
+//https://danthehandyman.github.io/uv-test2/
+//! NVIDIA login works using https://client.incog.dev/v1/ https://danthehandyman.github.io/uv-test2/_/hvtrs8%2F-wuw%2Cntific.aoo%2Fgn%2Fuq%2Feedopcg-lou%2F%3Djqo%3Fe%7BJhTo0kOkJlZo4kLAJSbJQkOkJkco91c0V%7BIkwkZ0NUIhokMk4uLhQ2LhEuNQIqIolicGliIhokIl0%3F
+//! get this site unblocked https://client.incog.dev/v1/
+//! github login thing with heroku as bare server works just takes a longggggggggg time getting lots of CORS errors
+//! uv-test-heroku client incog dev with https://client.incog.dev/v1/ as bare doesn't work Error occurred. Please try again later. loading plans
+
+
+//! use VERCEL maybe use https://client.incog.dev/v1/ on static hosting https://danthehandyman.github.io
+//! Vercel is unblocked!!!!
+//! could use heroku server as bare on vercel but prob not
+//! https://uv-test2.vercel.app/ and https://client.incog.dev/v1/ works!!!!!!!!!!!!
+//* https://git.holy.how/holy/website https://holy.how/v1/ https://git.holy.how/
+//* https://github.com/Titanium-Network/Holy-Unblocker
+//* h retro bowl and all the games every game source code wow wow!!!!!!! https://git.holy.how/holy/theatre/src/branch/master/public/html5/retrobowl/html5gamettps://github.com/Titanium-Network
+//! holy.how is blocked for the .how domain extension
+
+
+//!incognito links
+//X https://securlysucks.org/
+//birdseeds.xyz
+
+//* Holy Unblocker = https://uv.analystinn.net/v1/ https://uv.oguvod.org/
+//* Ludicrous https://ludicrous911.info/bare/
+//* Incognito https://doesmyfoodhavetasteornot.com/bare/
+//* General Mathematics https://fortnutbattlepass.ga/bare/
+//* Nebula https://schooltutor.xyz/bare/
+//* Utopia Unblocker https://math.utopia.education/bare/v1/
+//* General Mathematics https://fortnutbattlepass.ga/bare/
+
+//! links to test https://now.gg/play/roblox-corporation/5349/roblox https://play.geforcenow.com/mall/#/layout/games
+
+//! unblocked https://oguvod.org/ !!!!!! 9/16/22 in German though and CORS problems
+//! Germany error bare server very good thinks im in Germany error https://bare.illusionss.dev/ from https://github.com/tomphttp/bare-client/issues/1
+//! never mind weird error __uv is not defined
+
+//! https://client.incog.dev/ works well not in German :) but it's blocked 
+
+//! say thisisunsafe
+
+//! same UV url encoding
+
+
+//! very good about blank source https://www.utopiabeta.ml/js/changeSrc.js
+
+//! good that are blocked
+//* German https://bare.illusionss.dev/
+
+//* Not in German Shows GeForce Now plans https://lexiapowerup.cf/bare/ General Mathematics cf block 
+//! now.gg proxy detection https://danthehandyman.github.io/uv-test2/_/hvtrs8%2F-nmw%2Cge%2Frlcy-5%2Ce%3A600617.ahwni.hs
+//! now.gg different country detection https://danthehandyman.github.io/uv-test2/_/hvtrs8%2F-nmw%2Cge%2Frlcy-arp%2C3d006d22.hs
+
+//*https://math.utopiabeta.ga/bare/ Utopia Unblocker ga block
+
+//*https://uv.terbium.ga/ Holy Unblocker ga block
+
+//TODO conect the reponsive site to static uv site and connect github and try removing the now.gg scripts
+
+
